166ad6aac2079b55db4ab97e8a406301de115e9f
HumbertoGozzer <humbertogozzer@gmail.com>
2026-06-10 19:13:46 -0300

    Organizing folder of assets Creating Menu Insert assets of menu Creating the const file

---
+++ code/const.py
@@ -0,0 +1,14 @@
+#C
+COLOR_WHITE = (255, 255, 255)
+COLOR_ORANGE = (255, 128, 0)
+
+#M
+MENU_OPTION = ('NEW GAME',
+               'OPTIONS',
+               'SCORE',
+               'CREDITS',
+               'EXIT')
+
+#W
+WIN_WIDTH = 1200
+WIN_HEIGHT = 896
--- code/game.py
+++ code/game.py
@@ -2,16 +2,18 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.const import WIN_HEIGHT, WIN_WIDTH
 from code.menu import Menu
 
 
 class Game:
         def __init__(self):
             pygame.init()
-            self.window = pygame.display.set_mode(size=(600, 480))
+            self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
 
         def run(self):
+
             while True:
                 menu = Menu(self.window)
                 menu.run()
@@ -19,10 +21,5 @@ class Game:
 
 
 
-                # Check for all events
-                #for event in pygame.event.get():
-                #    if event.type == pygame.QUIT:
-                 #       pygame.quit() # Close Window
-                  #      quit() # End pygame
 
 
